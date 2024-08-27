6faeea04a2e8d1698c35665424dfba628c66563b
hmarques <helton.dias.oficial@gmail.com>
2024-08-27 12:51:06 -0300

    Entrega de exercicios nivel 2 - fase 4 - exercicio 4

--- src/nivel2_iniciante/fase4_funcoes_e_metodos/exer4_funcao_recursiva_para_calcular_fatorial/Main.java
+++ src/nivel2_iniciante/fase4_funcoes_e_metodos/exer4_funcao_recursiva_para_calcular_fatorial/Main.java
@@ -1,4 +1,29 @@
 package nivel2_iniciante.fase4_funcoes_e_metodos.exer4_funcao_recursiva_para_calcular_fatorial;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int fatorial, aux;
+        int resultado;
+
+        System.out.print("Insira o número que deseja saber o seu fatorial: ");
+        aux = scanner.nextInt();
+
+        fatorial = aux;
+        resultado = recursivo(fatorial);
+
+        System.out.print(aux + "! = "+ resultado);
+    }
+
+    public static int recursivo(int fatorial){
+
+        if(fatorial == 1){
+            return 1;
+        }
+
+        return fatorial * recursivo(fatorial - 1);
+    }
 }
