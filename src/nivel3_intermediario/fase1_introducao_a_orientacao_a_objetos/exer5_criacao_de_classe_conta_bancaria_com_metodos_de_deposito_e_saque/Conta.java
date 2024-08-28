133c06ab027aab2f45a720dfcacf7a1d2ae3d9ea
hmarques <helton.dias.oficial@gmail.com>
2024-08-28 12:34:31 -0300

    entrega de exercicios

--- src/nivel3_intermediario/fase1_introducao_a_orientacao_a_objetos/exer5_criacao_de_classe_conta_bancaria_com_metodos_de_deposito_e_saque/Conta.java
+++ src/nivel3_intermediario/fase1_introducao_a_orientacao_a_objetos/exer5_criacao_de_classe_conta_bancaria_com_metodos_de_deposito_e_saque/Conta.java
@@ -1,4 +1,19 @@
 package nivel3_intermediario.fase1_introducao_a_orientacao_a_objetos.exer5_criacao_de_classe_conta_bancaria_com_metodos_de_deposito_e_saque;
 
+import java.util.Scanner;
+
 public class Conta {
+    int valor;
+    Scanner scanner = new Scanner(System.in);
+    public int deposito(int saldo){
+        System.out.print("Insira o valor que deseja depositar: ");
+        valor = scanner. nextInt();
+        return saldo + valor;
+    }
+
+    public int saque(int saldo){
+        System.out.print("Insira o valor que deseja sacar: ");
+        valor = scanner. nextInt();
+        return saldo - valor;
+    }
 }
--- src/nivel3_intermediario/fase1_introducao_a_orientacao_a_objetos/exer5_criacao_de_classe_conta_bancaria_com_metodos_de_deposito_e_saque/Main.java
+++ src/nivel3_intermediario/fase1_introducao_a_orientacao_a_objetos/exer5_criacao_de_classe_conta_bancaria_com_metodos_de_deposito_e_saque/Main.java
@@ -1,7 +1,42 @@
 package nivel3_intermediario.fase1_introducao_a_orientacao_a_objetos.exer5_criacao_de_classe_conta_bancaria_com_metodos_de_deposito_e_saque;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        Conta conta = new Conta();
 
+        int saldo = 0;
+        int retry = 1;
+
+        while(retry != 0) {
+            int opcao;
+
+            System.out.println("Selecione uma opção" +
+                    "\n[1] Deposito" +
+                    "\n[2] Saque" +
+                    "\n[3] Consultar saldo");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    saldo = conta.deposito(saldo);
+                    System.out.print("O seu saldo atual é R$" + saldo);
+                    break;
+                case 2:
+                    saldo = conta.saque(saldo);
+                    System.out.print("O seu saldo atual é R$" + saldo);
+                    break;
+                case 3:
+                    System.out.print("O seu saldo atual é de R$" + saldo);
+                    break;
+                default:
+                    System.out.print("Opção invalida.");
+            }
+            System.out.print("\nCaso NÃO deseje retornar, digite 0: ");
+            retry = scanner.nextInt();
+        }
+        System.out.print("Obrigado por utilizar nosso banco!");
     }
 }
