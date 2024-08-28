6e77a0a582a4e9d289255d160deb2b558460b1e6
hmarques <helton.dias.oficial@gmail.com>
2024-08-28 17:57:43 -0300

    entrega de exercicios

--- src/nivel3_intermediario/fase2_introducao_a_arrays/exer6_contagem_de_elementos_pares_em_um_array/Main.java
+++ src/nivel3_intermediario/fase2_introducao_a_arrays/exer6_contagem_de_elementos_pares_em_um_array/Main.java
@@ -1,2 +1,36 @@
-package nivel3_intermediario.fase2_introducao_a_arrays.exer6_contagem_de_elementos_pares_em_um_array;public class Main {
+package nivel3_intermediario.fase2_introducao_a_arrays.exer6_contagem_de_elementos_pares_em_um_array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        String input;
+        int par = 0;
+        int impar = 0;
+
+        while (true){
+            System.out.print("Insira os números que você deseja: ");
+            input = scanner.nextLine();
+
+            if (input.isEmpty()){
+                break;
+            }
+            int num = Integer.parseInt(input);
+            numeros.add(num);
+        }
+
+        for(Integer atual : numeros){
+            if(atual%2==0){
+                par++;
+            }else{
+                impar++;
+            }
+        }
+        System.out.print("Par: "+ par +
+                        "\nImpar: "+ impar);
+    }
 }
--- src/nivel3_intermediario/fase2_introducao_a_arrays/exer7_ordenacao_de_um_array/Main.java
+++ src/nivel3_intermediario/fase2_introducao_a_arrays/exer7_ordenacao_de_um_array/Main.java
@@ -1,2 +1,35 @@
-package nivel3_intermediario.fase2_introducao_a_arrays.exer7_ordenacao_de_um_array;public class Main {
+package nivel3_intermediario.fase2_introducao_a_arrays.exer7_ordenacao_de_um_array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        String input;
+
+        while(true){
+            System.out.print("Insira os números(para encerrar não insira nada)");
+            input = scanner.nextLine();
+            if(input.isBlank()){
+                 break;
+            }
+            int num = Integer.parseInt(input);
+            numeros.add(num);
+        }
+
+        for(int i = 0; i < numeros.size(); i++){
+            for(int j = 0; j < numeros.size(); j++){
+                if(numeros.get(i) > numeros.get(j)){
+                    int aux1 = numeros.get(i);
+                    int aux2 = numeros.get(j);
+
+
+                }
+            }
+        }
+
+    }
 }
